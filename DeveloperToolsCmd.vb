69e54d23742e20fafdba558bfea9299505eaa3fa
chuong9x <31106432+chuong9x@users.noreply.github.com>
2020-06-20 12:48:08 +0700

    Update Funtion Run In Command Data

---
+++ DeveloperToolsCmd.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+
+Imports Autodesk.Revit.UI
+Imports Autodesk.Revit.DB
+Imports Autodesk.Revit.Attributes
+<Transaction(TransactionMode.Manual)>
+Public Class DeveloperToolsCmd
+    Implements IExternalCommand
+    Public Function Execute(commandData As ExternalCommandData, ByRef message As String, elements As ElementSet) As Result Implements IExternalCommand.Execute
+        Dim form As New DeveloperTools.KLHSnoop(commandData.Application.ActiveUIDocument)
+        form.Show()
+    End Function
+End Class
